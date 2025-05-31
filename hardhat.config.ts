682831bdb4524808555136099cb42e58aaa0dbb5
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-05-31 19:16:00 +0200

    added hardhat optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,29 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "hardhat-contract-sizer"
+
 require("@openzeppelin/hardhat-upgrades");
 require("dotenv").config();
 //Config ffrom: https://developers.flow.com/evm/quickstart
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
   networks: {
-    flow: {
-      url: 'https://mainnet.evm.nodes.onflow.org',
-      accounts: [process.env.DEPLOY_WALLET_1 as string],
-    },
-    flowTestnet: {
-      url: 'https://testnet.evm.nodes.onflow.org',
-      accounts: [process.env.DEPLOY_WALLET_1 as string],
-    },
+    // flow: {
+    //   url: 'https://mainnet.evm.nodes.onflow.org',
+    //   accounts: [process.env.DEPLOY_WALLET_1 as string],
+    // },
+    // flowTestnet: {
+    //   url: 'https://testnet.evm.nodes.onflow.org',
+    //   accounts: [process.env.DEPLOY_WALLET_1 as string],
+    // },
   },
   etherscan: {
     apiKey: {
