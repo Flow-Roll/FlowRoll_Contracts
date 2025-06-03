e0799d39af24f9a69d32a2fb0a1d67920d1b725b
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-06-03 20:53:43 +0200

    removed unused files

--- ignition/modules/FlowRollNFT.ts
+++
@@ -1,5 +0,0 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { parseEther } from "viem";
